0b3d84499e353e53571053172d4295638254253f
Victor <victorfsb@gmail.com>
2021-02-27 11:56:45 -0300

    Muda os ícones

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,13 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
